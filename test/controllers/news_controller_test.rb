44eb04c9f647f66a4acc640d47327c597e138d4b
yrmallu <yedukondalareddy@gmail.com>
2016-01-11 01:23:51 +0530

    HN search API tests

--- test/controllers/news_controller_test.rb
+++ test/controllers/news_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 class NewsControllerTest < ActionController::TestCase
   test "#index: successful news search" do
     test_json = File.read(Rails.root + 'test/lib/services/test_data/algolia_news.json')
-    stub_request(:get, "http://hn.algolia.com/api/v1/search").
+    stub_request(:get, "http://hn.algolia.com/api/v1/search?page=0").
           to_return(:status => 200, :body => test_json, :headers => {'Content-Type' => 'application/json'})
 
     api = Services::Algolia::News.new('v1')
